e5df99ce87ebaae6e6182b72dc98299672d99e44
marcos-german-diaz <germancho.diaz06@gmail.com>
2026-08-30 17:22:53 -0300

    feat: implementar CRUD en memoria y rutas de productos

---
+++ src/controllers/productos.controller.js
@@ -0,0 +1,81 @@
+let productos=[
+  { id: 1, nombre: "Poncho Catamarqueño de Vicuña", precio: 150000 },
+  { id: 2, nombre: "Cuenco de Cerámica Rustica", precio: 12500 },
+  { id: 3, nombre: "Cinturón de Cuero Crudo", precio: 18000 },
+  { id: 4, nombre: "Mate Imperial con Virola de Acero", precio: 30000}
+];
+
+export const siguienteId = () => productos.length > 0 ? Math.max(...productos.map(producto => producto.id)) + 1 : 1;
+
+// GET de productos
+export const obtenerProductos = (req,res) => {
+    return res.status(200).json(productos)
+};
+
+export const obtenerProductosFiltrados = (req,res) =>{
+    const {nombre} = req.query;
+    
+    if (!nombre) {
+    return res.status(200).json(productos)};
+  
+    const productosFiltrados = productos.filter((producto)=> producto.nombre.toLowerCase().includes(nombre.toLowerCase().trim())
+  );
+ 
+
+    res.status(200).json(productosFiltrados);
+};
+
+export const obtenerProductosporId = (req,res,next) => {
+     const id = parseInt(req.params.id);
+
+    const producto = productos.find(
+        p => p.id === id
+    );
+
+    if (!producto) {
+        const error = new Error("El evento no existe.");
+        error.status = 404;
+
+        return next(error);
+    }
+
+    res.json(producto);
+}
+
+// POST de productos
+
+export const crearProducto = (req,res,next)=>{
+     if (!req.body.nombre || !req.body.precio) {
+        const error = new Error(
+            "Los campos 'nombre' y 'precio' son obligatorios."
+        );
+
+        error.status = 400;
+
+        return next(error);
+    }
+
+    const nuevoProducto = {
+        id: siguienteId,
+        nombre: req.body.nombre,
+        precio: req.body.precio
+    };
+
+    productos.push(nuevoProducto);
+
+    res.status(201).json(nuevoEvento);
+};
+
+// DELETE de productos
+export const eliminarProducto = (req, res, next) => {
+    const id = Number(req.params.id);
+    
+    const indice = productos.findIndex(producto => producto.id === id);
+
+    if (indice === -1) {
+        return next(crearError(`no existe un producto con id ${req.id}`, 404));
+    }
+
+    productos.splice(indice, 1);
+    res.status(204).send();
+};
---
+++ src/routes/productos.routes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { obtenerProductos, obtenerProductosFiltrados, obtenerProductosporId, crearProducto, eliminarProducto } from '../controllers/productos.controller.js';
+
+const router = express.Router();
+
+router.get('/', obtenerProductos);
+router.get('/:id', obtenerProductosporId);
+router.get('/filtrados', obtenerProductosFiltrados);
+router.post('/', crearProducto);
+router.delete('/:id', eliminarProducto);
+
+export default router;
