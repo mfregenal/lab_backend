eb24a2c4fb9e1cc068d57409f9e895d376ffde61
marcos-german-diaz <germancho.diaz06@gmail.com>
2026-09-14 17:49:41 -0300

    fix: se cambio el mensaje de error en validarId

--- src/middlewares/validarIdMiddleware.js
+++ src/middlewares/validarIdMiddleware.js
@@ -4,7 +4,7 @@ export const validarId = (req, res, next) => {
     const id = Number(req.params.id);
 
     if (!Number.isInteger(id) || id <= 0) {
-        return next(crearError('El ID debe ser un número entero positivo', 400));
+        return next(crearError('ID Invalido', 400));
     }
 
     req.id = id;
