beeab548e7673d864628aac8f319eac88449b326
marcos-german-diaz <germancho.diaz06@gmail.com>
2026-09-13 21:40:46 -0300

    feat: se agrego los controladores y rutas de pabellon

---
+++ src/routes/pabellon.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { obtenerPabellones,obtenerPabellonPorId,eliminarPabellon,crearPabellon,actualizarPabellon } from "../controllers/pabellon.controller";
+import { validarId } from "../middlewares/validarId";
+
+const router = Router ();
+
+router.get('/',obtenerPabellones);
+router.get('/:id',validarId,obtenerPabellonPorId);
+router.post('/',crearPabellon);
+router.put('/:id', validarId, actualizarPabellon);
+router.delete('/:id',validarId,eliminarPabellon);
