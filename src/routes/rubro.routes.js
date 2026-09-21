a1b88a8ae87bfbdc42fa787f4fc85f55524f7abc
mfregenal <mfregenal23@gmail.com>
2026-09-21 02:32:04 -0300

    feat: se agrego validaciones por zod y modifico archivos correspondientes para su funcionamiento en rubro

--- src/routes/rubro.routes.js
+++ src/routes/rubro.routes.js
@@ -1,14 +1,14 @@
 import express from "express"
 import { obtenerRubros, obtenerRubroPorId, crearRubro, actualizarRubro, eliminarRubro } from "../controllers/rubro.controller.js"
 import { validarId } from "../middlewares/validarIdMiddleware.js"
-import { validarRubro } from '../middlewares/validarRubroMiddleware.js'
+import { validarCreacionRubro, validarActualizacionRubro, validarConsultaRubro } from '../middlewares/validarRubroMiddleware.js'
 
 const router = express.Router()
 
-router.get( '/', obtenerRubros )
-router.get( '/:id', validarId, obtenerRubroPorId )
-router.post( '/', validarRubro, crearRubro)
-router.put( '/:id', validarId, validarRubro, actualizarRubro )
-router.delete( '/:id', validarId, eliminarRubro )
+router.get('/', validarConsultaRubro, obtenerRubros)
+router.get('/:id', validarId, obtenerRubroPorId)
+router.post('/', validarCreacionRubro, crearRubro)
+router.put('/:id', validarId, validarActualizacionRubro, actualizarRubro)
+router.delete('/:id', validarId, eliminarRubro)
 
 export default router
